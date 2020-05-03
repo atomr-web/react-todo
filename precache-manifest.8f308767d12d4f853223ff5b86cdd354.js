210ac44c602a8c370a0827a170ccdd21101ac66d
atomr <atomr@MacBook-Air-atomr.local>
2020-05-03 17:58:23 +0500

    Updates

---
+++ precache-manifest.8f308767d12d4f853223ff5b86cdd354.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3bada52ac084d957ff8f622b8790874",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "4f211146a88ffcdda771",
+    "url": "/react-todo/static/css/main.975b0ed2.chunk.css"
+  },
+  {
+    "revision": "bc5640ac532b1053f5e4",
+    "url": "/react-todo/static/js/2.9b6fd2f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.9b6fd2f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f211146a88ffcdda771",
+    "url": "/react-todo/static/js/main.ffb9b466.chunk.js"
+  },
+  {
+    "revision": "c3f0068e021411733572",
+    "url": "/react-todo/static/js/runtime-main.fd60b479.js"
+  }
+]);
